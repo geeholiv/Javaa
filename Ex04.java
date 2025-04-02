0251c0fd8f41021061b44a11f52e5982b18a73b5
geeholiv <geovanna.oliveira1a@gmail.com>
2025-04-02 14:20:55 -0300

    aumento salario

---
+++ Ex04.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Ex 4 - Aumento salário
+
+*******************************************************************************/
+ import java.util.Scanner;
+public class Ex04
+{
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+       
+    System.out.print("Digite o salario: ");
+    double salario = scanner.nextDouble();
+    
+    System.out.print("Digite o tempo de trabalho: ");
+    double tempo = scanner.nextDouble();
+       
+
+    if (tempo>= 5 ) {
+    salario= salario*1.20;
+    System.out.println("O salario com o aumento de 20% será : "+salario);
+    
+    }else {
+    salario = salario * 1.10;
+    System.out.println("O salario com o aumento de 10% será: "+ salario);
+   
+     }
+     //saida
+     scanner.close();
+    }
+ }
