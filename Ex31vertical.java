ffc581c5088a2dcc6b5e16587062bf542bdfc51f
unknown <geovanna.oliveira1a@gmail.com>
2025-04-09 07:53:10 -0300

    Imprima na tela os números de 1 a 20, um abaixo do outro

---
+++ Ex31vertical.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+Ex 31 - Imprima na tela os números de 1 a 20, um abaixo do outro
+*******************************************************************************/
+public class Ex31vertical
+ {
+ 	public static void main(String[] args) {
+ 	    int numero = 0;
+ 	while(numero <= 19){
+ 	    numero++;
+ 	    System.out.println(+numero);
+ 	    
+ 	}
+ 	
+ 	}
+ }
+ 
